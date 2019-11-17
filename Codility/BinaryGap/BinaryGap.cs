e6fc6928d1461043ac03422fb6919720ca3fa5c2
Etnic <kubus.martin@gmail.com>
2019-11-17 19:02:55 +0100

    Add project files.

---
+++ Codility/BinaryGap/BinaryGap.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Codility.BinaryGap
+{
+    public static class BinaryGap
+    {
+        public static int solution(int N)
+        {
+            var binaryNumber = Convert.ToString(N, 2).ToCharArray();
+            var counter = 0;
+            var list = new List<int>();
+
+            for (int i = 1; i < binaryNumber.Length; i++)
+            {
+                if (binaryNumber[i] == '1')
+                {
+                    list.Add(counter);
+                    counter = 0;
+                    continue;
+                }
+
+                counter++;
+            }
+
+            if (list.Count() == 0)
+                list.Add(0); 
+                    
+            return list.Max();
+        }
+    }
+}
---
+++ Codility/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Codility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BinaryGap.BinaryGap.solution(32));
+        }
+    }
+}
